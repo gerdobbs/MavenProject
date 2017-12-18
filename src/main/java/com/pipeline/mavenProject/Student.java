4744d3cdd9e2293d9c292480a0871d6e8a5b0085
gerdobbs <dobbsger0@gmail.com>
2017-12-18 19:12:20 +0000

    Updated Student.java file

--- src/main/java/com/pipeline/mavenProject/Student.java
+++ src/main/java/com/pipeline/mavenProject/Student.java
@@ -10,9 +10,8 @@ public class Student {
     public AttendanceGrade getAttendanceGrade(int attendance) {
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
         if (attendance < 30) return AttendanceGrade.VERY_POOR;
-        if (attendance < 50) return AttendanceGrade.POOR;
         if (attendance < 70) return AttendanceGrade.AVERAGE;
-        if (attendance <= 90) return AttendanceGrade.GOOD;
+        if (attendance < 90) return AttendanceGrade.GOOD;
         return AttendanceGrade.VERY_GOOD;
     }
 }
